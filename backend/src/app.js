a3b5b33dcff66c35d78268ff6186bdbd3ba5f295
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-03-01 03:47:35 +0800

    added routes for posts and walls, imported postsrouter and wallsrouters

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,12 @@
 //TODO: change ES5 syntax to ES6
-var express = require('express');
+
+import express from 'express';
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import postsRouter from './routes/postsRouter';
+import wallsRouter from './routes/wallsRouter';
 
 var app = express();
 
@@ -13,9 +14,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/', (req, res) =>
+  res.send('hello world')
+)
+
+app.use('/posts', postsRouter)
+app.use('/walls', wallsRouter)
 
 module.exports = app;
