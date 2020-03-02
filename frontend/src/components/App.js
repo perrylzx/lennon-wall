02c8b33d1a03e74463f68baae557230d66041f42
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-03-02 14:05:56 +0800

    add parent presentational component

---
+++ frontend/src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import AddPost from '../containers/AddPost'
+
+// TODO(PERRY): learn router to add more parameter from url
+const App = () => (
+  <div>
+    <AddPost />
+  </div>
+)
+export default App
